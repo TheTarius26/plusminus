c61879fffb66985eebedfbe9b7f613c67240b3f5
Rhaka Novyansyah Dewanto <rhakadewanto26@gmail.com>
2022-03-08 14:10:40 +0700

    remove padding inside cell

--- lib/widgets/cell.dart
+++ lib/widgets/cell.dart
@@ -10,10 +10,7 @@ class Cell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TableCell(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(child: content),
-      ),
+      child: content,
     );
   }
 }
